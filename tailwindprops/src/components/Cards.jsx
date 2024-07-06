e5b621fac93112c0c77d978d03fb3705a587945b
Diviya Pandiyan <kdmail9@gmail.com>
2024-07-06 22:29:44 +0530

    add file

--- tailwindprops/src/components/Cards.jsx
+++ tailwindprops/src/components/Cards.jsx
@@ -4,8 +4,7 @@ const Cards = (props) => {
     // console.log(props)
   return (
     <figure className="md:flex bg-slate-100 rounded-xl p-8 md:p-0 dark:bg-slate-800">
-    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="../assets/good.png" alt="The head and torso of a dinosaur skeleton;
-          it has a large head with long sharp teeth" width="384" height="512"/>
+    <img className="w-24 h-24 md:w-48 md:h-auto md:rounded-none rounded-full mx-auto" src="https://www.istockphoto.com/photo/group-of-people-on-peak-mountain-climbing-helping-team-work-travel-trekking-success-gm1009803562-272216359?utm_campaign=srp_photos_top&utm_content=https%3A%2F%2Funsplash.com%2Fs%2Fphotos%2Ffree-images&utm_medium=affiliate&utm_source=unsplash&utm_term=free+images%3A%3A%3A" alt="" width="384" height="512"/>
     <div className="pt-6 md:p-8 text-center md:text-left space-y-4">
       <blockquote>
         <p className="text-lg font-medium">
