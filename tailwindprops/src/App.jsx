28d2d63ecfc87b0433d1883e6e0e6c809c217f61
Diviya Pandiyan <kdmail9@gmail.com>
2024-07-06 09:09:46 +0530

    first commit

---
+++ tailwindprops/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Cards from './components/Cards'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Cards username="Diviya"/>
+      <Cards/>
+      <Cards/>
+      <Cards/>
+    </>
+  )
+}
+
+export default App
